503a128356be88b92a488161039def70c3f633be
owenglahn <orglahn@gmail.com>
2021-05-14 15:43:41 -0400

    Add add_diet()

--- app.py
+++ app.py
@@ -4,25 +4,28 @@ from time import sleep
 from flask import Flask, redirect, render_template, request, url_for
 from bs4 import BeautifulSoup as soup
 
+import json
 app = Flask(__name__, template_folder='template', static_folder='static')
-
 
 user_input = {'meal': '', 'diet_restrictions': []}
 others = []
 
-
 @app.route('/', methods=["GET", "POST"])
 def index():
-    user_input['diet_restrictions'] = request.form.getlist('diet_button')
-    user_input['meal'] = request.form.get('meal')
+    if request.method == 'GET':
+        diet = request.form.get('diet_button')
+        return redirect(url_for('add_diet', diet=diet))
     return render_template('index.html')
 
+@app.route('/<diet>', methods=["GET"])
+def add_diet(diet):
+    user_input.getlist('diet_restrictions').append(diet)
+    return render_template('index.html')
 
 @app.route('/add_other', methods=['POST'])
 def add_other():
     others.append(request.form.get('other'))
     return render_template('index.html', others=others)
-
 
 @app.route('/results')
 def results():
@@ -30,6 +33,7 @@ def results():
     for rest in user_input['diet_restrictions']:
         search_string += rest + ';'
     # use google search engine
+    
     search_string += user_input.get('meal') + ';recipe'
     driver = webdriver.Chrome()
     driver.get("https://www.google.com/")
@@ -38,12 +42,10 @@ def results():
     # search with user input as keywords
     google_search.send_keys(search_string)
     google_search.send_keys(Keys.ENTER)
-    # sleep(4)
     assert "No results found." not in driver.page_source
-    # get page source
+    # web scrape results from search
     recipes = soup(driver.page_source, 'html.parser').find_all('a')[5:]
     driver.quit()
-    # web scrape results from search
     return render_template('results.html', recipes=recipes, search_string=search_string)
 
 if __name__ == '__main__':
