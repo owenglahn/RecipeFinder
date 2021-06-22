8ae652ac3a1c767ad86f5f6c87b33f93e8ecf6e5
Owen Glahn <owenglahn@Owens-MacBook-Air.local>
2021-06-22 11:49:00 -0400

    First workingversion

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 from flask import Flask, redirect, render_template, request, url_for
 from bs4 import BeautifulSoup as soup
+import requests
 
 import json
 app = Flask(__name__, template_folder='template', static_folder='static')
@@ -10,22 +11,26 @@ app = Flask(__name__, template_folder='template', static_folder='static')
 user_input = {'meal': '', 'diet_restrictions': []}
 others = []
 
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    if request.method == 'GET':
-        diet = request.form.get('diet_button')
-        return redirect(url_for('add_diet', diet=diet))
+    if request.method == 'POST':
+        user_input['meal'] = request.form.get('meal')
+        user_input['diet_restrictions'] = request.form.getlist('diet_button')
+        return redirect(url_for('results'))
     return render_template('index.html')
+
 
 @app.route('/results')
 def results():
     search_string = ''
     for rest in user_input['diet_restrictions']:
         search_string += rest + ';'
-    # use google search engine
-    
+
     search_string += user_input.get('meal') + ';recipe'
+    # depends on safari being installed and safaridriver being enabled
     driver = webdriver.Chrome()
+    # use google search engine
     driver.get("https://www.google.com/")
     google_search = driver.find_element_by_name("q")
     google_search.clear()
@@ -34,9 +39,10 @@ def results():
     google_search.send_keys(Keys.ENTER)
     assert "No results found." not in driver.page_source
     # web scrape results from search
-    recipes = soup(driver.page_source, 'html.parser').find_all('a')[5:]
+    recipes = soup(driver.page_source, 'html.parser').find_all('a')[30:50]
     driver.quit()
     return render_template('results.html', recipes=recipes, search_string=search_string)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
